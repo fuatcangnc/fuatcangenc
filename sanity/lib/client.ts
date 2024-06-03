b3f54552ead475f0c7d8ab4ffdbbad3872a1400a
Fuat <fuatcankod@gmail.com>
2024-06-03 16:09:38 +0300

    commit message here

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,4 +1,5 @@
-import { createClient } from 'next-sanity'
+import 'server-only'
+import { createClient, QueryParams } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
@@ -6,6 +7,24 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn,
+  useCdn:true,
   perspective: 'published',
 })
+
+
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  tags,
+}: {
+  query: string
+  params?: QueryParams
+  tags?: string[]
+}) {
+  return client.fetch<QueryResponse>(query, params, {
+    next: {
+      revalidate: 30, // for simple, time-based revalidation
+      tags, // for tag-based revalidation
+    },
+  })
+}
